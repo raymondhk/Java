f13724539f70c7980ca451a1022efa3b77660f86
Raymond Kim <ray.kim09@gmail.com>
2018-01-08 20:18:51 -0500

    update

---
+++ Beloved/src/main/java/com/project/Beloved/BelovedApplication.java
@@ -0,0 +1,11 @@
+package com.project.Beloved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelovedApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(BelovedApplication.class, args);
+	}
+}
